83c0759d55ceff19508cdf68dd9e19f1d1b35680
Teaksters <arne.meijs@student.uva.nl>
2023-03-07 15:13:21 +0100

    trying to add age to the data

--- src/simplexai/experiments/tabular_mimic.py
+++ src/simplexai/experiments/tabular_mimic.py
@@ -71,7 +71,7 @@ def load_tabular_mimic(random_seed: int = 42) -> tuple:
         i += 1
 
     general_df.rename(columns={'SUBJECT_ID': 'stay'}, inplace=True)
-    print(general_df)
+    print(list(general_df))
     exit()
     ##################################################################
 
