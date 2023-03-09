aadc83e305f7535f0d2dad44f41751e713102592
Teaksters <arne.meijs@student.uva.nl>
2023-03-09 16:34:36 +0100

    coding

--- src/simplexai/experiments/time_series_mimic.py
+++ src/simplexai/experiments/time_series_mimic.py
@@ -169,7 +169,7 @@ def load_time_series_mimic(random_seed: int = 42) -> tuple:
         temp_df = pd.read_csv(path)
         for col in temporal_features:
             print(df['stay'])
-            print(df.loc[df['stay'] == stay], stay)
+            print(df.loc[df['stay'] == stay, col], stay, col)
 
             # df.loc[df['stay'] == stay, col]
             # print(list(temp_df))
