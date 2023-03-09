9baa41fb88394e4239c47218c263a7f34e7bf67d
Teaksters <arne.meijs@student.uva.nl>
2023-03-09 17:09:47 +0100

    coding

--- src/simplexai/experiments/time_series_mimic.py
+++ src/simplexai/experiments/time_series_mimic.py
@@ -159,8 +159,8 @@ def load_time_series_mimic(random_seed: int = 42) -> tuple:
 
     # Load tabular data
     df = load_tabular_mimic()
-    df = pd.concat([df, pd.DataFrame(columns=temporal_features)])
-    df[temporal_features] = df[temporal_features].astype('object')
+    df = pd.concat([df, pd.DataFrame(columns=temporal_features, dtype='object')])
+    # df[temporal_features] = df[temporal_features].astype('object')
     print(df)
     print(df.dtypes)
 
