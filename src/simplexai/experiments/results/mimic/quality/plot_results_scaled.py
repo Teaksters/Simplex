ffe00ae2cd92d47fafc38656ca8d55beff96bc44
Teaksters <arne.meijs@student.uva.nl>
2023-03-22 16:08:10 +0100

    working on creating the plotting code

--- src/simplexai/experiments/results/mimic/quality/plot_results_scaled.py
+++ src/simplexai/experiments/results/mimic/quality/plot_results_scaled.py
@@ -135,9 +135,10 @@ min_df = results_df.groupby(["explainer", "n_keep", "scaler"]).aggregate("min").
 max_df = results_df.groupby(["explainer", "n_keep", "scaler"]).aggregate("max").unstack(level=0)
 q1_df = results_df.groupby(["explainer", "n_keep", "scaler"]).quantile(0.25).unstack(level=0)
 q3_df = results_df.groupby(["explainer", "n_keep", "scaler"]).quantile(0.75).unstack(level=0)
-results_df = results_df.groupby(["explainer", "n_keep", "scaler"]).unstack(level=0)
+results_df = results_df.groupby(["explainer", "n_keep", "scaler"])
 
 print(results_df)
+print(list(results_df))
 exit()
 
 for m, metric_name in enumerate(metric_names):
