609c1e775ae529163cddd295db923b7437f8b46d
Teaksters <arne.meijs@student.uva.nl>
2023-03-28 11:26:18 +0200

    prep plotting

--- src/simplexai/experiments/results/mimic/quality/plot_results2.py
+++ src/simplexai/experiments/results/mimic/quality/plot_results2.py
@@ -64,12 +64,12 @@ params = {"text.latex.preamble": r"\usepackage{amsmath}"}
 plt.rcParams.update(params)
 representer_metrics = np.zeros((2, len(cv_list)))
 current_path = Path.cwd()
-load_path = current_path / "experiments/results/mimic/quality" / 'scaled'
+load_path = current_path / "experiments/results/mimic/quality/scaled/"
 
 for scaler in scalers:
     for cv in cv_list:
         classifier = MortalityPredictor(n_cont=1, input_feature_num=26)
-        classifier.load_state_dict(torch.load(load_path / f"model_cv{cv}.pth"))
+        classifier.load_state_dict(torch.load(load_path / str(scaler) / f"model_cv{cv}.pth"))
         classifier.to(device)
         classifier.eval()
         for n_keep in n_keep_list:
