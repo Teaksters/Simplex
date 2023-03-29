42d1404c257e9067d48e50c4d20ed3f0d197a7a9
Teaksters <arne.meijs@student.uva.nl>
2023-03-29 12:28:56 +0200

    trying boxplots

--- src/simplexai/experiments/results/mimic/quality/plot_results_box.py
+++ src/simplexai/experiments/results/mimic/quality/plot_results_box.py
@@ -138,7 +138,7 @@ for m, metric_name in enumerate(metric_names):
                                    (results_df['scaler'] == scaler)]
         data.append(list(temp_data[metric_name])) # Maybe this is wrong..?
     plt.boxplot(data)
-    plt.xticks([i for i in range(1, len(scalers))], scalers)
+    plt.xticks([i for i in range(1, len(scalers) + 1)], scalers)
 
 safe_path = load_path / 'plots3/'
 if not os.path.exists(safe_path):
