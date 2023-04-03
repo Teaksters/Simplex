3aa4def7e8e186e5a7c3cf4d06428b5ae2b3f9a1
Teaksters <arne.meijs@student.uva.nl>
2023-04-03 12:14:45 +0200

    final version I think

--- src/simplexai/experiments/results/mimic/outlier/plot_results_OODcorpus.py
+++ src/simplexai/experiments/results/mimic/outlier/plot_results_OODcorpus.py
@@ -181,7 +181,7 @@ plt.fill_between(
 )
 plt.xlabel("Number of samples inspected")
 plt.ylabel("Number of outliers detected")
-plt.legend(fontsize='small')
+plt.legend(loc='upper left', fontsize='x-small')
 
 plt.savefig(safe_path / "outlier2.pdf", bbox_inches="tight")
 
