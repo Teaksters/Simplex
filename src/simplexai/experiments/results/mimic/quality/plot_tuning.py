35e4f44332730b95d2cf16669c6632bc703b2e48
Teaksters <arne.meijs@student.uva.nl>
2023-04-07 14:14:08 +0200

    tuning plot

---
+++ src/simplexai/experiments/results/mimic/quality/plot_tuning.py
@@ -0,0 +1,23 @@
+import argparse
+import pickle as pkl
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import sklearn.metrics
+import torch
+
+from simplexai.models.tabular_data import MortalityPredictor
+
+load_path = current_path / "experiments/results/mimic/quality/tuning/epochs"
+
+for epoch_path in os.listdir(load_path):
+    temp_path = load_path / epoch_path
+    for data_path in os.listdir(temp_path) if data[-4:] == '.pkl':
+        temp_path = temp_path / data_path
+        file = open(temp_path, 'rb')
+        data = pkl.load(file)
+        print(epoch_path, data_path, data)
+        exit()
