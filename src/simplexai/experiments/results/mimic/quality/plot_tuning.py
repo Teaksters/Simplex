f3f4c01276f22ff1a041cbb6e4d3727f85a76cf2
Teaksters <arne.meijs@student.uva.nl>
2023-04-07 14:35:01 +0200

    gathered the data in place

--- src/simplexai/experiments/results/mimic/quality/plot_tuning.py
+++ src/simplexai/experiments/results/mimic/quality/plot_tuning.py
@@ -16,12 +16,22 @@ from simplexai.models.tabular_data import MortalityPredictor
 current_path = Path.cwd()
 load_path = current_path / "experiments/results/mimic/quality/tuning/epochs"
 
+train_losses = []
+test_losses = []
+test_accs = []
+epochs = []
 for epoch_path in os.listdir(load_path):
+    epochs.append(int(epoch_path))
     temp_path = load_path / epoch_path
     for data_path in os.listdir(temp_path):
         if data_path[-4:] == '.pkl':
             temp_path = temp_path / data_path
             file = open(temp_path, 'rb')
             data = pkl.load(file)
-            print(epoch_path, data_path, data)
-            exit()
+            train_losses.append(data[0])
+            test_losses.append(data[2])
+            test_accs.append(data[3])
+
+print('train_losses:\n', train_losses)
+print('test_losses:\n', test_losses)
+print('test_accs:\n', test_accs)
