700535e4e6b5e1ab193f2d4ea238c609e3771dec
Teaksters <arne.meijs@student.uva.nl>
2023-04-12 10:18:59 +0200

    trying to load without cuda

--- src/simplexai/experiments/results/mimic/quality/plot_tuning.py
+++ src/simplexai/experiments/results/mimic/quality/plot_tuning.py
@@ -13,6 +13,13 @@ import torch
 from simplexai.models.tabular_data import MortalityPredictor
 
 
+class CPU_Unpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module == 'torch.storage' and name == '_load_from_bytes':
+            return lambda b: torch.load(io.BytesIO(b), map_location='cpu')
+        else: return super().find_class(module, name)
+
+
 current_path = Path.cwd()
 load_path = current_path / "experiments/results/mimic/quality/tuning/epochs"
 
@@ -26,12 +33,11 @@ for epoch_path in os.listdir(load_path):
     for data_path in os.listdir(temp_path):
         if data_path[-4:] == '.pkl':
             temp_path = temp_path / data_path
-            with torch.loading_context(map_location='cpu'):
-                file = open(temp_path, 'rb')
-                data = pkl.load(file)
-                train_losses.append(data[0])
-                test_losses.append(data[2])
-                test_accs.append(data[3])
+            file = open(temp_path, 'rb')
+            data = CPU_Unpickler(file).load()
+            train_losses.append(data[0])
+            test_losses.append(data[2])
+            test_accs.append(data[3])
 
 train_losses = np.array(train_losses)
 test_losses = np.array(test_losses)
