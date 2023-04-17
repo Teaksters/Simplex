ab11933cec7345512c191c62612abaea65187726
Teaksters <arne.meijs@student.uva.nl>
2023-04-17 14:23:51 +0200

    getting ready for auc measurements

--- src/simplexai/experiments/tabular_model_tuning.py
+++ src/simplexai/experiments/tabular_model_tuning.py
@@ -129,7 +129,7 @@ def approximation_quality(
                     test_loss += F.nll_loss(output, target, reduction="sum").item()
                     pred = output.data.max(1, keepdim=True)[1]
                     correct += pred.eq(target.data.view_as(pred)).sum()
-                    probas.append(probs)
+                    probas = torch.cat((probas, probs), -1)
 
             print(probas)
             exit()
