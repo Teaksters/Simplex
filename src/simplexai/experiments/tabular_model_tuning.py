8a6ab24c1889467b2346bfa17ac33ea1c880bbde
Teaksters <arne.meijs@student.uva.nl>
2023-04-17 15:06:30 +0200

    getting ready for auc measurements

--- src/simplexai/experiments/tabular_model_tuning.py
+++ src/simplexai/experiments/tabular_model_tuning.py
@@ -138,6 +138,7 @@ def approximation_quality(
             preds = torch.cat(preds, 0)
             preds = preds.cpu().detach().numpy().flatten()
             preds[-1] = 1
+            print(preds, probas[preds])
             auc_score = sklearn.metrics.roc_auc_score(preds, probas[preds])
             print(auc_score)
             exit()
