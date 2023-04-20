cfe16e6e270ad9c8d5bdbd231b9a4d27608125ec
Teaksters <arne.meijs@student.uva.nl>
2023-04-20 16:04:10 +0200

    attempt to plot tSNE of logits

--- src/simplexai/experiments/results/mimic/quality/plot_logits_tSNE.py
+++ src/simplexai/experiments/results/mimic/quality/plot_logits_tSNE.py
@@ -75,6 +75,7 @@ df = pd.DataFrame(data_dict)
 
 # Reduce logits to 2 dimensional space using tSNE reduction
 tsne = TSNE(n_components=2, verbose=1, random_state=42)
+print(df['logits'].shape, df['logits'].numpy())
 tsne_z = tsne.fit_transform(df['logits'])
 df['z'] = tsne_z
 print(df)
