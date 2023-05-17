41ac34d0aa4fbfd011b5234cc62740cf2a7ef9a7
Teaksters <arne.meijs@student.uva.nl>
2023-05-17 13:56:27 +0200

    printing or plotting tuning data

--- src/simplexai/experiments/results/mimic/quality/plot_tuning.py
+++ src/simplexai/experiments/results/mimic/quality/plot_tuning.py
@@ -38,7 +38,7 @@ for epoch_path in os.listdir(load_path):
             temp_path = cur_path / data_path
             file = open(temp_path, 'rb')
             data = CPU_Unpickler(file).load()
-            print(data)
+            print(data, data_path)
             train_losses.append(data[0])
             train_counter = data[1]
             test_losses.append(data[2])
