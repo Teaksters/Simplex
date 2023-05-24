8f897024b1dde5bb66db2973c3b69772113a2e02
Teaksters <arne.meijs@student.uva.nl>
2023-05-24 19:32:41 +0200

    getting ready for testing again once data is preprocessed

--- src/simplexai/experiments/corpus_generation.py
+++ src/simplexai/experiments/corpus_generation.py
@@ -30,29 +30,24 @@ def main():
     X_df, y_df = load_tabular_mimic()
     # Replace all place holders for nan with nan again for later calculations
     X_df[X_df <= 0] = np.nan
-    print(X_df)
-    print(list(X_df.columns), list(X_df.columns)[-25:])
-    exit()
+
     ######################### SO FAR SO GOOD #####################3
     diagnoses_df = X_df.iloc[:, -25:]
-    # print(list(X_df), list(diagnoses_df))
+    print(diagnoses_df.shape)
 
     # Interpret grand scale patterns
     diagnoses = list(diagnoses_df)
     occurances = diagnoses_df.sum()
     diagnoses_probs = occurances / occurances.sum()
-    # print(diagnoses_probs)
-    # print(list(X_df.iloc[:, -25:]))
-    # exit()
+    print(diagnoses, diagnoses_probs, diagnoses_probs.shape)
 
-    # Generate diagnoses prototypes using averaging and thresholding
-    # prototype_df = pd.DataFrame(columns=list(X_df))
+    # Generate diagnoses prototypes using mean values for each diagnosis
     prototype_df = []
     for diagnosis in diagnoses:
         single_df = X_df.loc[X_df[diagnosis] == 1.]
         prototype = single_df.mean()
 
-        # Normalize diagnoses into probability function
+        # Normalize binary diagnoses features into probability function
         prototype.iloc[-25:] /= prototype.iloc[-25:].sum()
         prototype = dict(prototype)
         prototype_df.append(prototype)
