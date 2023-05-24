6f6e1522ce71200445bb942cc7a42d1fd75a4237
Teaksters <arne.meijs@student.uva.nl>
2023-05-24 21:25:16 +0200

    getting ready for testing again once data is preprocessed

--- src/simplexai/experiments/corpus_generation.py
+++ src/simplexai/experiments/corpus_generation.py
@@ -19,21 +19,15 @@ from simplexai.experiments.tabular_mimic import MimicDataset, \
                                                 load_tabular_mimic
 
 
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_DIR = '../Data/preprocessed'
+DATA_DIR = Path('../Data/preprocessed')
 
 
-
-
-def main():
-    # Load the data
-    X_df, y_df = load_tabular_mimic()
+def create_corpus(data, n_bin=25):
     # Replace all place holders for nan with nan again for later calculations
-    X_df.iloc[:, :-25][X_df.iloc[:, :-25] <= 0] = np.nan
+    X_df.iloc[:, :-n_bin][X_df.iloc[:, :-n_bin] <= 0] = np.nan
+    diagnoses_df = X_df.iloc[:, -n_bin:]
 
-    diagnoses_df = X_df.iloc[:, -25:]
-
-    # Interpret grand scale patterns
+    # Calculate occurance probabilities (thresholds)
     diagnoses = list(diagnoses_df)
     occurances = diagnoses_df.sum()
     diagnoses_probs = occurances / occurances.sum()
@@ -44,19 +38,26 @@ def main():
         single_df = X_df.loc[X_df[diagnosis] == 1.]
         prototype = single_df.mean()
 
-        ######################### SO FAR SO GOOD #####################3
         # Normalize binary diagnoses features into probability function
-        prototype.iloc[-25:] /= prototype.iloc[-25:].sum()
+        prototype.iloc[-n_bin:] /= prototype.iloc[-n_bin:].sum()
         prototype = dict(prototype)
         prototype_df.append(prototype)
 
     # Binarize diagnosis features using the occurance probability as threshold
     prototype_df = pd.DataFrame.from_dict(prototype_df)
-    prototype_df.iloc[:, -25:] = (prototype_df.iloc[:, -25:] >= diagnoses_probs).astype('float')
-    for row in prototype_df:
-        print(row, '\n', prototype_df[row])
-    print(prototype_df)
+    prototype_df.iloc[:, -n_bin:] = (prototype_df.iloc[:, -n_bin:] >= diagnoses_probs).astype('float')
+    return prototype_df
 
+def main():
+    # Load the data
+    X_df, _ = load_tabular_mimic()
+    prototypes = create_corpus(X_df)
+
+    # Store the created corpus
+    corpus_path = DATA_DIR / 'pickles' / 'tab_mort_diagnosis_prototypes.pkl'
+    print('Done, storing corpus for later.')
+    with open(corpus_path, "wb") as f:
+        pkl.dump(prototypes, f)
     return 0
 
 
