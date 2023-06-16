2a22327477914af278455e86992376dd80c445c8
Teaksters <arne.meijs@student.uva.nl>
2023-06-16 13:16:20 +0200

    why it no work?

--- src/simplexai/experiments/tabular_scaling_mimic.py
+++ src/simplexai/experiments/tabular_scaling_mimic.py
@@ -563,7 +563,7 @@ def outlier_detection2(
                     test_loss += F.nll_loss(output, target, reduction="sum").item()
                     pred = output.data.max(1, keepdim=True)[1]
                     correct += pred.eq(target.data.view_as(pred)).sum()
-                    print(test_loss)
+                    print(output, target)
             test_loss /= len(test_loader.dataset)
             test_losses.append(test_loss)
             print(
