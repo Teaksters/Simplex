651efb32515a987a3fad821eaac819a05c001ea9
Teaksters <arne.meijs@student.uva.nl>
2023-06-16 14:31:59 +0200

    trying to get the normalization to work again

--- src/simplexai/models/tabular_data.py
+++ src/simplexai/models/tabular_data.py
@@ -27,9 +27,9 @@ class MortalityPredictor(BlackBox):
 
     def latent_representation(self, x: torch.Tensor) -> torch.Tensor:
         x_cont, x_disc = x[:, : self.n_cont], x[:, self.n_cont :]
-        print(x_cont)
-        x_cont = self.bn1(x_cont)
-        print(x_cont)
+        print('before: ', x_cont)
+        temp = self.bn1(x_cont)
+        print('temp: ', temp)
         x = torch.cat([x_cont, x_disc], 1)
         x = F.relu(self.lin1(x))
         x = self.drops(x)
