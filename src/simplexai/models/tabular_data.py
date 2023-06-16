cf42069d7e2246f1af78b7861627be774f832d13
Teaksters <arne.meijs@student.uva.nl>
2023-06-16 13:33:26 +0200

    why it no work?

--- src/simplexai/models/tabular_data.py
+++ src/simplexai/models/tabular_data.py
@@ -28,10 +28,11 @@ class MortalityPredictor(BlackBox):
 
     def latent_representation(self, x: torch.Tensor) -> torch.Tensor:
         x_cont, x_disc = x[:, : self.n_cont], x[:, self.n_cont :]
+        print(x)
         if self.n_cont > 0:
-            print('normalizing')
             x_cont = self.bn1(x_cont)
         x = torch.cat([x_cont, x_disc], 1)
+        print(x)
         x = F.relu(self.lin1(x))
         print(x)
         x = self.drops(x)
