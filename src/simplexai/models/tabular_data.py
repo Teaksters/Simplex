8a4af10922075b9e90744f8e489abeee46ca527a
Teaksters <arne.meijs@student.uva.nl>
2023-06-30 12:14:54 +0200

    debugging is starting

--- src/simplexai/models/tabular_data.py
+++ src/simplexai/models/tabular_data.py
@@ -26,8 +26,11 @@ class MortalityPredictor(BlackBox):
         return x
 
     def latent_representation(self, x: torch.Tensor) -> torch.Tensor:
+        print(x)
         x_cont, x_disc = x[:, : self.n_cont], x[:, self.n_cont :]
         x_cont = self.bn1(x_cont)
+        print(x_cont)
+        exit()
         x = torch.cat([x_cont, x_disc], 1)
         x = F.relu(self.lin1(x))
         x = self.drops(x)
