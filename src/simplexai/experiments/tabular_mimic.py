f47b2d4cd8ae9670ccab3c8feb75be467a83788a
Teaksters <arne.meijs@student.uva.nl>
2023-06-30 17:18:40 +0200

    Gotcha

--- src/simplexai/experiments/tabular_mimic.py
+++ src/simplexai/experiments/tabular_mimic.py
@@ -151,11 +151,9 @@ def load_timeseries(): # COULD BE USED FOR MORE VALUES LATER BY NOT DROPPING THO
             # For singular measurements have special handling
             if col == 'Weight' or col == 'Height':
                 if episode_df[col].notnull().values.any():
-                    print('mean: \n', episode_df[col], episode_df[col].mean())
                     episode_dict_col = {col: episode_df[col].mean()}
                     episode_dict.update(episode_dict_col)
                 else:
-                    print('nan: \n', print(episode_df[col]), 0.0)
                     episode_dict_col = {col: 0.0}
                     episode_dict.update(episode_dict_col)
                 continue
